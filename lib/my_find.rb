a800cc6f04cb9abe5a5cb33d19c8dd51ff4851db
Nicole Dow <nikki.dow71@gmail.com>
2019-07-15 21:56:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,12 +1,7 @@
-require 'pry'
-
 def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
-    end
-    i += 1
+    i = i + 1
   end
 end
-
-#my_find(collection) {|i| i % 3 == 0 && o % 5 == 0}
